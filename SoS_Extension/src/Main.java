b8d46f8a01188cd1cb1f3fb4b73efc2d93d10bae
abalon1210 <abalon1210@daum.net>
2019-11-03 22:47:17 -0800

    Commit Test

--- SoS_Extension/src/Main.java
+++ SoS_Extension/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	    // Generate Random Scenario
+	    // Generate Random Scenario 
 	    ScenarioGenerator scenarioGenerator = new ScenarioGenerator();
         scenarioGenerator.generateRandomScenario(2);
 
