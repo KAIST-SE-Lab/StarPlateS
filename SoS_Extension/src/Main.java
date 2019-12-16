c7376802788cfabb2f71a7217e77118f25996026
abalon1210 <abalon1210@daum.net>
2019-12-15 23:41:50 -0800

    Interplay Model Parser Implementation - Interplay Model Parser - Interplay Model Printer

--- SoS_Extension/src/Main.java
+++ SoS_Extension/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         boolean isSMBFL = false;
         boolean isBMBFL = false;
-        boolean isIMBFL = false;
+        boolean isIMBFL = true;
         if (args.length == 0) {
             System.out.println("Usage: java main <switch> [<file>]");
             System.out.println("Where: <switch> = -structure or -smbfl");
@@ -46,7 +46,7 @@ public class Main {
 //        BehaviorModelBasedFaultLocalization bmfl TODO create new class
 //        InterplayModelBasedFautLocalization imfl TODO create new class
 
-        for(int i = 1; i <= 3; i+=2) { // Number of scenarios, currently 50
+//        for(int i = 1; i <= 3; i+=2) { // Number of scenarios, currently 50
 //            try {
 //                BufferedReader reader = new BufferedReader(new FileReader(omnetConf));
 //
@@ -76,85 +76,86 @@ public class Main {
 //                System.out.println(e);
 //            }
 
-            Random r = new Random();
-            int accidentThreshold = (int)(1000 * 0.01) + r.nextInt(10);
+//            Random r = new Random();
+//            int accidentThreshold = (int)(1000 * 0.01) + r.nextInt(10);
+//
+//            for(int j = 0; j < 1; j++) { // Number of execution of same scenarios & configurations
+//                String s;
+//                Runtime rt = Runtime.getRuntime();
+//                System.out.println("RUNNING");
+//
+//                if(j == accidentThreshold) {
+//                    System.out.println("CHANGED");
+//                    File addNode = new File("./examples/platoon_SoS/addNode.xml");
+//                    String content = "";
+//                    try {
+//                        BufferedReader br = new BufferedReader(new FileReader(addNode));
+//
+//                        String line = br.readLine();
+//                        //System.out.println(line);
+//
+//                        while (line != null) {
+//                            if(line.contains("stopped")) {
+//                                System.out.println(line);
+//                                content += "\n";
+//                            }
+//                            else content += line + "\n";
+//                            line = br.readLine();
+//                        }
+//
+//                        br.close();
+//
+//                        FileWriter writer = new FileWriter(addNode);
+//                        writer.write(content);
+//
+//                        writer.close();
+//                    } catch (Exception e) {
+//                        System.out.println(e);
+//                    }
+//                }
+//
+//                try { // Execute a process and loggin the console output
+//                    System.setOut(new PrintStream(new FileOutputStream("./examples/platoon_SoS/results/consoleLog.txt")));
+//                    System.out.println("####### Scenario: " + i + " #######");
+//                    Process p = rt.exec("opp_run -m -u Cmdenv -c Platooning -n ..:../../src -l ../../src/VENTOS_Public omnetpp.ini", null, new File("./examples/platoon_SoS"));
+//
+//                    // Logging the process result
+//                    BufferedReader br = new BufferedReader(
+//                            new InputStreamReader(p.getInputStream()));
+//                    while ((s = br.readLine()) != null) {
+//                        System.out.println("line: " + s);
+//                    }
+//                    p.waitFor();
+//                    System.out.println("exit: " + p.exitValue());
+//                    p.destroy();
+//                } catch (Exception e) {
+//                    System.out.println(e);
+//                }
+//
+//                // Logging the data of platooning & vehicle emission
+//                File plnConfig = new File("./examples/platoon_SoS/results/000_plnConfig.txt");
+//                File plnData = new File("./examples/platoon_SoS/results/000_plnData.txt");
+//                File emissionData = new File("./examples/platoon_SoS/results/000_vehicleEmission.txt");
+//                File vehData = new File("./examples/platoon_SoS/results/000_vehicleData.txt");
+//
+////                boolean isTracePassed=false; //TODO get the simulation result.
+//                // issue 1 insert fault vehicle?
+//                if (isSMBFL)
+//                    smbfl.structureModelOverlapping(false, i, j);
+//                if (isBMBFL);
+////                    bmbfl.
+//                if (isIMBFL);
+////                    imbfl.
+//                System.out.println(plnConfig.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"plnConfig.txt")));
+//                plnData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"plnData.txt"));
+//                emissionData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"emissionData.txt"));
+//                vehData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"vehicleData.txt"));
+//
+////                StructureModel stm = new StructureModel(i, j);
+////                stm.drawGraph();
+//            }
+//        }
 
-            for(int j = 0; j < 1; j++) { // Number of execution of same scenarios & configurations
-                String s;
-                Runtime rt = Runtime.getRuntime();
-                System.out.println("RUNNING");
-
-                if(j == accidentThreshold) {
-                    System.out.println("CHANGED");
-                    File addNode = new File("./examples/platoon_SoS/addNode.xml");
-                    String content = "";
-                    try {
-                        BufferedReader br = new BufferedReader(new FileReader(addNode));
-
-                        String line = br.readLine();
-                        //System.out.println(line);
-
-                        while (line != null) {
-                            if(line.contains("stopped")) {
-                                System.out.println(line);
-                                content += "\n";
-                            }
-                            else content += line + "\n";
-                            line = br.readLine();
-                        }
-
-                        br.close();
-
-                        FileWriter writer = new FileWriter(addNode);
-                        writer.write(content);
-
-                        writer.close();
-                    } catch (Exception e) {
-                        System.out.println(e);
-                    }
-                }
-
-                try { // Execute a process and loggin the console output
-                    System.setOut(new PrintStream(new FileOutputStream("./examples/platoon_SoS/results/consoleLog.txt")));
-                    System.out.println("####### Scenario: " + i + " #######");
-                    Process p = rt.exec("opp_run -m -u Cmdenv -c Platooning -n ..:../../src -l ../../src/VENTOS_Public omnetpp.ini", null, new File("./examples/platoon_SoS"));
-
-                    // Logging the process result
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(p.getInputStream()));
-                    while ((s = br.readLine()) != null) {
-                        System.out.println("line: " + s);
-                    }
-                    p.waitFor();
-                    System.out.println("exit: " + p.exitValue());
-                    p.destroy();
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                // Logging the data of platooning & vehicle emission
-                File plnConfig = new File("./examples/platoon_SoS/results/000_plnConfig.txt");
-                File plnData = new File("./examples/platoon_SoS/results/000_plnData.txt");
-                File emissionData = new File("./examples/platoon_SoS/results/000_vehicleEmission.txt");
-                File vehData = new File("./examples/platoon_SoS/results/000_vehicleData.txt");
-
-//                boolean isTracePassed=false; //TODO get the simulation result.
-                // issue 1 insert fault vehicle?
-                if (isSMBFL)
-                    smbfl.structureModelOverlapping(false, i, j);
-                if (isBMBFL);
-//                    bmbfl.
-                if (isIMBFL);
-//                    imbfl.
-                System.out.println(plnConfig.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"plnConfig.txt")));
-                plnData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"plnData.txt"));
-                emissionData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"emissionData.txt"));
-                vehData.renameTo(new File("./examples/platoon_SoS/results/" + i + "_" + j +"vehicleData.txt"));
-
-//                StructureModel stm = new StructureModel("./logs/", i, j);
-//                stm.drawGraph();
-            }
-        }
         if (isSMBFL) {
             ArrayList<EdgeInfo> edgeInfos = smbfl.SMcalculateSuspiciousness();
             StructureModel finalSM = new StructureModel();
@@ -173,7 +174,8 @@ public class Main {
         }
 
         if (isIMBFL) {
-
+            InterplayModel interplayModel = new InterplayModel(1, 0);
+            interplayModel.printSequence();
         }
     }
 }
