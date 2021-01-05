075042b99397d0fcfd09dd6656bf74da0c1d93dd
abalon1210 <abalon1210@daum.net>
2021-01-05 21:44:07 +0900

    Experiment 1) - Base

--- SoS_Extension/src/Main.java
+++ SoS_Extension/src/Main.java
@@ -250,7 +250,7 @@ public class Main {
             ArrayList<ArrayList<String>> oracle = oracleGenerator.getOracle();
 
             if (isClustering && !single) {
-                File file2 = new File(base + "/SoS_Extension/results/" + "F1P - 1) HyperparameterAnalysis_Case6_MINLEN.csv");  // TODO Which Case? -> File Name Change
+                File file2 = new File(base + "/SoS_Extension/results/" + "F1P - 1) HyperparameterAnalysis_Base.csv");  // TODO Which Case? -> File Name Change
                 try {
                     FileWriter writer = new FileWriter(file2, true);
                     String ret = "";
@@ -264,9 +264,9 @@ public class Main {
 
                                 for (InterplayModel im : IMs) {
                                     // 대조군 Clustering Algorithm
-//                                    clustering.addTraceBaseLCS(im, delay_threshold, lcs_min_len_threshold);
+                                    clustering.addTraceBaseLCS(im, delay_threshold, lcs_min_len_threshold);
 
-                                    clustering.addTraceCase6(im, simlr_threshold, delay_threshold, lcs_min_len_threshold);
+//                                    clustering.addTraceCase6(im, simlr_threshold, delay_threshold, lcs_min_len_threshold);
 
                                     // For Merging&Finalizing Optimization
 //                                    clustering.addTraceCase5(im, c_simlr, c_delay, c_len);
