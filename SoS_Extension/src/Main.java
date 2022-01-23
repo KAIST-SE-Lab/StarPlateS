a333f4e332cd3fcfe7a7d2e14d7cc46c65b5be00
abalon1210 <abalon1210@daum.net>
2022-01-23 12:48:39 +0900

    Modify the experiment setting to Case 6 ML

--- SoS_Extension/src/Main.java
+++ SoS_Extension/src/Main.java
@@ -275,10 +275,10 @@ public class Main {
             double m_delay = 1;
             int m_len = 9;
 
-            for (int k = 1; k < 20; k++) {
+            for (int k = 0; k < 1; k++) {
                 ArrayList<InterplayModel> IMs_batch = new ArrayList<>();
 //
-                File batch_im = new File(base + "/SoS_Extension/results/" + "F1P - HyperparameterAnalysis_Base_" + (k+11) + ".txt");
+                File batch_im = new File(base + "/SoS_Extension/results/" + "F1P - HyperparameterAnalysis_Case6_ML" + (k+14) + ".txt");
                 try {
                     FileWriter writer = new FileWriter(batch_im, true);
                     String ret = "";
@@ -375,7 +375,7 @@ public class Main {
 //                File folder2 = new File(base + "/SoS_Extension/results/patterns/" + formatter.format(date));
 //                folder2.mkdir();
                         if (!single_run) {
-                            File file2 = new File(base + "/SoS_Extension/results/" + "F1P - HyperparameterAnalysis_Base_" + (k+11) + ".csv");  // TODO Which Case? -> File Name Change
+                            File file2 = new File(base + "/SoS_Extension/results/" + "F1P - HyperparameterAnalysis_Case6_ML" + (k+14) + ".csv");  // TODO Which Case? -> File Name Change
 //                    File file2 = new File(base + "/SoS_Extension/results/" + "F1P - Base HyperparameterAnalysis_withTime_03_19.csv");
                             try {
                                 FileWriter writer = new FileWriter(file2, true);
@@ -393,20 +393,20 @@ public class Main {
 //                                    Collections.shuffle(IMs); // TODO Random Sort
                                             for (InterplayModel im : IMs_batch) {
                                                 // 대조군 Clustering Algorithm
-                                                clustering.addTraceBaseLCS(im, delay_threshold, lcs_min_len_threshold);
+//                                                clustering.addTraceBaseLCS(im, delay_threshold, lcs_min_len_threshold);
 
-//                                            clustering.addTraceCase5(im, simlr_threshold, delay_threshold, lcs_min_len_threshold);
+                                            clustering.addTraceCase6(im, simlr_threshold, delay_threshold, lcs_min_len_threshold);
 
                                                 // For Merging&Finalizing Optimization
 //                                    clustering.addTraceCase6(im, c_simlr, c_delay, c_len);
                                             }
 
                                             // Clustering Merge Optimization
-//                                        clustering.ClusterMerge(simlr_threshold, delay_threshold, lcs_min_len_threshold);
+                                        clustering.ClusterMerge(simlr_threshold, delay_threshold, lcs_min_len_threshold);
 
                                             // Clustering Finalize Optimization
 //                                clustering.ClusterMerge(m_simlr, m_delay, m_len);
-//                                        clustering.ClusteringFinalize(simlr_threshold, delay_threshold, lcs_min_len_threshold);
+                                        clustering.ClusteringFinalize(simlr_threshold, delay_threshold, lcs_min_len_threshold);
 
                                             // Pattern Logging
 //                                    File folder3 = new File(base + "/SoS_Extension/results/patterns/" + formatter.format(date) + "/" + simlr_threshold*100 + "_" + delay_threshold*100 + "_" + lcs_min_len_threshold);
